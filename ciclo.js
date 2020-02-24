5047eac78ab44a8db19a99b8ca6ccb7f03dfd326
Zoilo Granda <zgranda@gmail.com>
2020-02-24 10:31:40 -0400

    06 Ciclo de vida de Vue (Lifecycle)

---
+++ ciclo.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		saludo: 'ciclo de vida',
+	},
+ beforeCreate(){
+  console.log('beforeCreate');
+ },
+ created(){
+  console.log('created');
+ },
+ beforeMount(){
+  console.log('beforeMount');
+ },
+ mounted(){
+  console.log('mounted');
+ },
+ beforeUpdate(){
+  console.log('beforeUpdate');
+ },
+ updated(){
+  console.log('updated');
+ },
+ beforeDestroy(){
+  console.log('beforeDestroy');
+ },
+ destroyed(){
+  console.log('destroyed');
+ },
+ methods:{
+  destruir(){
+   this.$destroy()
+  }
+ }
+})
