ee265c0da46c1500d3ebc67a096fa37bd609334c
Zoilo Granda <zgranda@gmail.com>
2020-02-27 12:35:39 -0400

    09 Comunicación entre componentes (parte 2)

--- lecciones/componentes2/hijo.js
+++ lecciones/componentes2/hijo.js
@@ -2,7 +2,16 @@ Vue.component('hijo',{
  template:`
  <div class="py-5 bg-success">
  <h4>Componente Hijo: {{numero}}</h4>
+ <h4>Nombre: {{nombre}}</h4>
  </div>
  `,
- props:['numero']
+ props:['numero'],
+ data(){
+  return {
+   nombre: 'Pepe'
+  }
+ },
+ mounted(){
+  this.$emit('nombreHijo', this.nombre)
+ }
 })
