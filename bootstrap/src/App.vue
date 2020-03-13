7bd421fdbfc743e293800f3781b2a3bfa1e86458
Zoilo Granda <zgranda@gmail.com>
2020-03-13 14:16:46 -0400

    22 Navbar [Class Active] Bootstrap 4 + Vue

--- bootstrap/src/App.vue
+++ bootstrap/src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+ <div id="nav">
+  <b-navbar toggleable="lg" type="dark" variant="danger">
+   <b-container>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-collapse is-nav id="nav-collapse">
+     <b-navbar-nav>
+        <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+        <b-nav-item :to="{name: 'About'}">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+   </b-container>
+  </b-navbar>
+ </div>
+ <router-view />
+</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+ padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+ font-weight: bold;
+ color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+ color: #42b983;
 }
 </style>
