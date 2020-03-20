24c37580d1752b61f729ce71e72b8d7657c03f8a
Zoilo Granda <zgranda@gmail.com>
2020-03-20 08:58:20 -0400

    26 Formulario Select y Radio Buttons con Bootstrap 4 + Vue

--- bootstrap/src/views/Form.vue
+++ bootstrap/src/views/Form.vue
@@ -3,11 +3,23 @@
    <h1>Formularios</h1>
    <form>
     <b-row>
-     <b-col md="6">
+     <b-col md="3">
       <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
       <small>Minimo 3 caracteres</small>
       <p>texto: {{texto}}</p>
      </b-col>
+    <b-col md="3">
+     <b-form-select v-model="select" :options="animales"></b-form-select>
+     <p>Seleccionado: {{select}}</p>
+    </b-col>
+    <b-col md="3">
+     <b-form-radio-group v-model="radio" :options="animalesRadio"></b-form-radio-group>
+     <p>Seleccionado: {{radio}}</p>
+    </b-col>
+    <b-col md="3">
+     <b-form-checkbox-group v-model="check" :options="animalesRadio"></b-form-checkbox-group>
+     <p>Seleccionado: {{check}}</p>
+    </b-col>
     </b-row>
    </form>
   </div>
@@ -18,7 +30,21 @@ export default {
  name: 'Form',
  data(){
   return {
-   texto:''
+   texto:'',
+   check:[],
+   select:null,
+   animales:[
+    {value:null, text:'Seleccione un animal'},
+    {value:'perro', text:'perro perro'},
+    {value:'gato', text:'gato gato'},
+    {value:'pato', text:'pato pato'}
+   ],
+   radio:null,
+   animalesRadio:[
+    {value:'perro', text:'perro perro'},
+    {value:'gato', text:'gato gato'},
+    {value:'pato', text:'pato pato'}
+   ]
   }
  },
  computed:{
