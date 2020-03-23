36ebe62e34440d4de498c5a43eb16fbed132671d
Zoilo Granda <zgranda@gmail.com>
2020-03-23 09:38:06 -0400

    2 VUETIFY - Framework Options [Grid/Colores/Tipografía/Espaciados]

--- vuetify1/src/views/Home.vue
+++ vuetify1/src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+     <v-layout wrap>
+      <v-flex class="primary" xs12>
+       <h1>12</h1>
+      </v-flex>
+      <v-flex class="secondary" xs6>
+       <h1>6</h1>
+      </v-flex>
+      <v-flex class="error" xs6>
+       <h1>6</h1>
+      </v-flex>
+     </v-layout>
+     <v-layout class="secondary" mt-5 justify-center align-center style="height:300px">
+      <v-flex class="info" xs3>
+       <h1>Centrado</h1>
+      </v-flex>
+      <v-flex class="pink lighten-1 white--text" xs3>
+       <h1>Centrado</h1>
+      </v-flex>
+     </v-layout>
+     <v-layout>
+      <v-flex>
+       <h1 class="display-1 text-uppercase">Algun titulo</h1>
+       <h1 class="display-2 pa-5 mt-5">Algun titulo</h1>
+       <h1 class="display-3 text-center">Algun titulo</h1>
+       <h1 class="display-4">Algun titulo</h1>
+      </v-flex> 
+     </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
