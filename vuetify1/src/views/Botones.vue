e96148a9300636e802e677d9694b4490b3b15d42
Zoilo Granda <zgranda@gmail.com>
2020-03-24 08:37:44 -0400

    3 VUETIFY - Botones e Iconos

---
+++ vuetify1/src/views/Botones.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+ <v-container>
+  <v-row>
+   <v-col><v-btn>Texto</v-btn></v-col>
+   <v-col><v-btn color="primary">Texto</v-btn></v-col>
+   <v-col><v-btn dark color="blue">Texto</v-btn></v-col>
+   <v-col><v-btn color="secondary" text>Sin Fondo</v-btn></v-col>
+   <v-col><v-btn color="warning" large>Largo</v-btn></v-col>
+   <v-col><v-btn color="warning" small>Pequeño</v-btn></v-col>
+  </v-row>
+  <v-row>
+    <v-col><v-btn color="warning" large disabled>Disabled</v-btn></v-col>
+    <v-col><v-btn color="info" depressed>Depressed</v-btn></v-col>
+    <v-col><v-btn><v-icon>favorite</v-icon></v-btn></v-col>
+    <v-col><v-btn color="primary" icon text><v-icon>forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="primary" fab><v-icon>forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="primary" fab small><v-icon>forum</v-icon></v-btn> </v-col>
+  </v-row>
+  <v-row>
+    <v-col><v-btn color="primary" fab large><v-icon>forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="success">Texto<v-icon>forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="success">Texto<v-icon class="ml-2">forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="success">Texto<v-icon right>forum</v-icon></v-btn></v-col>
+    <v-col><v-btn color="success"><v-icon left>forum</v-icon>Texto</v-btn></v-col>
+    <v-col><v-btn color="success" outlined><v-icon left>forum</v-icon>Texto</v-btn></v-col>
+  </v-row>
+  <v-row>
+  <v-col><v-btn color="success" outlined block><v-icon left>forum</v-icon>Texto</v-btn></v-col>
+ </v-row>
+ </v-container>
+</template>
+
+<script>
+export default {
+ name: 'Botones'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
