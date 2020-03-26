f01e65a35c6450e1e6b0f14b738c7134f887dc65
Zoilo Granda <zgranda@gmail.com>
2020-03-26 09:28:59 -0400

    5 VUETIFY - Card Material Design

--- vuetify1/src/router/index.js
+++ vuetify1/src/router/index.js
@@ -25,6 +25,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "botones" */ '../views/Botones.vue')
+  },
+  {
+    path: '/cards',
+    name: 'Cards',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "cards" */ '../views/Cards.vue')
   }
 ]
 
---
+++ vuetify1/src/views/Cards.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+ <v-container grid-list-md>
+  <v-row wrap>
+   <v-col cols="6">
+    <v-card>
+     <v-img src="https://picsum.photos/300/200"></v-img>
+     <v-card-title> <h2>Titulo</h2> </v-card-title>
+     <v-card-text> <p>asldjaslkdj  q3809327 ashdashd saidhasd 2893d 2d2dj hkjhsakdh sa98d sadasdjsahd  73udiwdjkajsdasd y  jhjhkjh</p> </v-card-text>
+     <v-card-actions><v-btn color="info" text>Accion</v-btn></v-card-actions>
+    </v-card>
+   </v-col>
+   <v-col cols="6">
+    <v-card>
+     <v-img src="https://picsum.photos/300/200"></v-img>
+    </v-card>
+   </v-col>
+  </v-row>
+ </v-container>
+</template>
+
+<script>
+export default {
+ name: 'Cards'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
