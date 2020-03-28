68b9c18111362d9e0901392f293db3a56565cb70
Zoilo Granda <zgranda@gmail.com>
2020-03-28 10:01:18 -0400

    Vuetify versión 2.x - Actualización GRID

---
+++ vuetify-comp/src/views/Grid.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+ <v-container>
+  <v-row>
+   <v-col cols="12" sm="6">
+    <p>askldjs adioasu d9s7das98 dsadasdsadas dsadsajhdksajhdkas 89sa7d98sa7 dsajdhsakdhsajdshd sdhsjdhjsdj ss djsh djshd jsdh</p>
+   </v-col>
+   <v-col cols="12" sm="6">
+    <p>askldjs adioasu d9s7das98 dsadasdsadas dsadsajhdksajhdkas 89sa7d98sa7 dsajdhsakdhsajdshd sdhsjdhjsdj ss djsh djshd jsdh</p>
+   </v-col>
+  </v-row>
+ </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
