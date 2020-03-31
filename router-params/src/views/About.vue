8cd154ee89865fd64341ab699980a0c80670f2b6
Zoilo Granda <zgranda@gmail.com>
2020-03-31 10:08:42 -0400

    VUE JS | Cómo configurar Query Params en Router-Link

---
+++ router-params/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{query:{seccion:1}}" class="btn btn-primary btn-block">1</router-link>
+    <router-link :to="{query:{seccion:2}}" class="btn btn-primary btn-block">2</router-link>
+  </div>
+</template>
+<script>
+ export default {
+  data(){
+   return{
+    
+   }
+  },
+  watch:{
+   "$route.query.seccion":{
+    immediate:true,
+    handler(seccion){
+     console.log(`seccion ha cambiado ${seccion}`);
+    }
+   }
+  }
+ }
+</script>
